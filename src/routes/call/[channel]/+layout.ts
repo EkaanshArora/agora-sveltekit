12544a098f040cb9d447dc7e1e60e1902f2c090f
EkaanshArora <ekaansh68@gmail.com>
2023-03-31 17:30:33 +0530

    feat: use envVarsInUse

--- src/routes/call/[channel]/+layout.ts
+++ src/routes/call/[channel]/+layout.ts
@@ -3,4 +3,5 @@ export const ssr = false;
 /** @type {import('@sveltejs/adapter-vercel').Config} */
 export const config = {
   runtime: 'edge',
+  envVarsInUse: ['PUBLIC_APP_ID', 'APP_CERTIFICATE'],
 };
